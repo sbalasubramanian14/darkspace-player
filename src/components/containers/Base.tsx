f70dee0641950be3361268b922eb5459633eafc5
Bala <sbalasubramanian96@gmail.com>
2019-11-14 08:58:33 +0530

    Basic Routing

--- src/components/containers/Base.tsx
+++ src/components/containers/Base.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import DarkPlayer from "./DarkPlayer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const Base = () => <DarkPlayer />;
+const Base = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={DarkPlayer} />
+      <Route exact path="/:activeVideo" component={DarkPlayer} />
+      {/* Other route not found component */}
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Base;
